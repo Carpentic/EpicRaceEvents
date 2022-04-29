60b9686968c011d4b6f3861d887e41ea2a185d73
Nicolas de Bengy <nicolas.debengy@ynov.com>
2022-04-29 11:04:21 +0200

    Drivers: Add drivers list and paging

---
+++ App/Controllers/DriversController.cs
@@ -0,0 +1,20 @@
+∩╗┐using App.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class DriversController : Controller
+    {
+        IRepository _repository;
+
+        public DriversController(IRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            return View(new Models.ViewModels.Drivers.IndexModel(_repository, page));
+        }
+    }
+}
---
+++ App/Models/ViewModels/Drivers/IndexModel.cs
@@ -0,0 +1,25 @@
+∩╗┐using App.Data;
+using App.Models.DatabaseModels;
+
+namespace App.Models.ViewModels.Drivers;
+
+public class IndexModel
+{
+    IRepository _repository;
+    public List<Driver> Drivers { get; set; }
+    public PagingInfo PagingInfo { get; set; }
+
+    public IndexModel(IRepository repo, int page)
+    {
+        _repository = repo;
+        var local_drivers = _repository.GetAll<Driver>().ToList();
+
+        PagingInfo = new PagingInfo
+        {
+            CurrentPage = page,
+            ItemsPerPage = 25,
+            TotalItems = local_drivers.Count
+        };
+        Drivers = local_drivers.Skip((page - 1) * PagingInfo.ItemsPerPage).Take(PagingInfo.ItemsPerPage).ToList();
+    }
+}
